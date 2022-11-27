e9f2657c748911f8da0e5059eee77959fcc335d1
ilknur yilmaz <ilknuray1318@gmail.com>
2022-11-27 22:14:52 +0100

    Api

--- src/test/java/base_urls/RestfulBaseUrl.java
+++ src/test/java/base_urls/RestfulBaseUrl.java
@@ -11,6 +11,7 @@ public class RestfulBaseUrl {
     @Before
     public void setup(){
 
+
         spec = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com").build();
     }
 }
